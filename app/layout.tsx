d5a94ee47f5ca69d9882608cd843be5a151cc262
Yuyu S <swsalim@gmail.com>
2024-04-01 15:07:05 +0800

    fix simple analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,8 @@ export default function RootLayout({
     >
       <head>
         <link rel="preconnect" href="//ik.imagekit.io" />
+        <link rel="preconnect" href="//stats.wateraday.com" />
+        <link rel="dns-prefetch" href="//stats.wateraday.com" />
         <link rel="dns-prefetch" href="//ik.imagekit.io" />
         <Script
           id="simple-analytics"
@@ -101,6 +103,7 @@ export default function RootLayout({
             referrerPolicy="no-referrer-when-downgrade"
             width="1"
             height="1"
+            unoptimized
           />
         </noscript>
       </body>
